782ecd65e1ae5a2a3927a5c89281f143b9d5993c
Mykyta Voronyi <nick.voronoy@gmail.com>
2024-07-06 00:19:44 -0400

    update nvim

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -6,6 +6,8 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.relativenumber = true
 vim.opt.undofile = true
 vim.opt.signcolumn = "yes"
+vim.opt.pumheight = 4
+vim.o.ignorecase = true
 
 -- vim.o.mouse = "a"
 -- vim.o.scrolloff = 999
@@ -18,3 +20,19 @@ vim.keymap.set("v", "<C-s>", "<Esc>:w<CR>", { noremap = true, silent = true })
 -- Split windows
 vim.keymap.set("n", "<leader>ws", "<Cmd>split<CR>", { noremap = true, silent = false })
 vim.keymap.set("n", "<leader>wv", "<Cmd>vsplit<CR>", { noremap = true, silent = false })
+
+-- Disable arrow keys
+vim.keymap.set("n", "<Up>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Down>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Left>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Right>", "<Nop>", { noremap = true, silent = true })
+
+-- Optionally, disable them in insert and visual mode as well
+vim.keymap.set("i", "<Up>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("i", "<Down>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("i", "<Left>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("i", "<Right>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("v", "<Up>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("v", "<Down>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("v", "<Left>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("v", "<Right>", "<Nop>", { noremap = true, silent = true })
--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -14,14 +14,13 @@ return {
 		config = function()
 			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 			local cmp = require("cmp")
+			local luasnip = require("luasnip")
 			require("luasnip.loaders.from_vscode").lazy_load()
-
 			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-
 			cmp.setup({
 				snippet = {
 					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
+						luasnip.lsp_expand(args.body)
 					end,
 				},
 				window = {
@@ -34,11 +33,25 @@ return {
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "llm-ls" },
-					{ name = "luasnip" }, -- For luasnip users.
+					{ name = "luasnip" },
 				}, {
 					{ name = "buffer" },
 				}),
